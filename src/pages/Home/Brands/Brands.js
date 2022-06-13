e6ba90f9d70db917a3bda7c1ddd6757f8881a200
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-06-14 00:18:32 +0600

    Home page brands complete.

---
+++ src/pages/Home/Brands/Brands.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import style from "./Brands.module.css";
+import logo1 from "../../../assets/Google.png";
+import logo2 from "../../../assets/Airbnb.png";
+import logo3 from "../../../assets/Amazon2.png";
+import logo4 from "../../../assets/Apple.png";
+import logo5 from "../../../assets/Spotify.png";
+import logo6 from "../../../assets/Nike.png";
+import { Container } from "react-bootstrap";
+
+const Brands = () => {
+    const settings = {
+        // dots: true,
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        // autoplaySpeed: 1000,
+        cssEase: "linear"
+      };
+  return (
+    <div className={style.brand__container}>
+      <Container>
+        <Slider {...settings}>
+          <div>
+            <img src={logo1} alt="" />
+          </div>
+          <div>
+            <img src={logo2} alt="" />
+          </div>
+          <div>
+            <img src={logo3} alt="" />
+          </div>
+          <div>
+            <img src={logo4} alt="" />
+          </div>
+          <div>
+            <img src={logo5} alt="" />
+          </div>
+          <div>
+            <img src={logo6} alt="" />
+          </div>
+        </Slider>
+      </Container>
+    </div>
+  );
+};
+
+export default Brands;
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import NavBar from '../NavBar/NavBar';
 import Header from "../Header/Header";
 import style from './Home.module.css'
+import Brands from '../Brands/Brands';
 
 const Home = () => {
     return (
         <div>
             <NavBar/>
             <Header/>
+            <Brands/>
         </div>
     );
 };
