8910d45c2393c857b9fc92ab80486550736b2cb6
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-06-16 17:28:56 +0600

    404 page not found page complete.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import NotFound from "./pages/components/NotFound/NotFound";
 import Home from "./pages/Home/Home/Home";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="*" element={<NotFound/>} />
       </Routes>
     </div>
   );
---
+++ src/pages/components/NotFound/NotFound.js
@@ -0,0 +1,39 @@
+import React from "react";
+import notFound from "../../../assets/notFound2.jpg";
+import notFound1 from "../../../assets/notFound1.gif";
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import style from "./NotFound.module.css";
+
+const NotFound = () => {
+  return (
+    <div className={style.notFound__container}>
+      <Container>
+        <Row className={`${style.notFound} shadow-lg rounded`}>
+          <Col>
+            <div className={style.notFound__img}>
+              <img src={notFound} alt="img" />
+            </div>
+          </Col>
+          <Col>
+            <div className={style.notFound__content}>
+              <h2>Oops</h2>
+              <h4>404 - Page Not Found</h4>
+              <p>
+                The page you're looking for might have been removed has it's
+                name changed or is temporary unavailable.
+              </p>
+              
+                <Link to="/home" className="button">
+                  Back To Home
+                </Link>
+              
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default NotFound;
