60b7762448d5634a8223b0fedd4c711b5fac0fac
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-06-16 12:56:19 +0600

    Home Query section complete.

--- src/pages/Home/HomeQuery/HomeQuery.js
+++ src/pages/Home/HomeQuery/HomeQuery.js
@@ -1,8 +1,68 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import style from "./HomeQuery.module.css";
 
 const HomeQuery = () => {
+  const handleWebsiteUrlSubmit = (event) => {
+    event.preventDefault();
+    const website = event.target.website.value;
+    const email = event.target.email.value;
+    const information = { website, email };
+
+    if (!email && !website) {
+      errorInformation();
+    } else if (!email) {
+      errorEmail();
+    } else if (!website) {
+      errorWebsite();
+    } else {
+      success();
+      event.target.reset();
+    }
+  };
+  const success = () =>
+    toast.success("Successfully Send.", {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  const errorInformation = () =>
+    toast.error("Please Fill This Field.", {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  const errorEmail = () =>
+    toast.error("Please Enter Your Email", {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  const errorWebsite = () =>
+    toast.error("Please Enter Your Website url", {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+
   return (
     <div>
       <Container>
@@ -15,14 +75,39 @@ const HomeQuery = () => {
                 different sizes.
               </p>
             </div>
-            <div className={style.home__query__inputs}>
-              <input type="text" placeholder="Enter Website url" />
-              <input type="email" placeholder="Enter Your Email" />
-              <button className='button'>send query</button>
-            </div>
+            <form onSubmit={handleWebsiteUrlSubmit}>
+              <div className={style.home__query__inputs}>
+                <input
+                  name="website"
+                  // required
+                  autoComplete="off"
+                  type="text"
+                  placeholder="Enter Website url"
+                />
+                <input
+                  name="email"
+                  // required
+                  autoComplete="off"
+                  type="email"
+                  placeholder="Enter Your Email"
+                />
+                <button className="button">Send Query</button>
+              </div>
+            </form>
           </Col>
         </Row>
       </Container>
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 };
