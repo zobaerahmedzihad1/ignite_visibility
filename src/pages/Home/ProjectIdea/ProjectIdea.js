d98d2e89419c01ed5dfd41e52cb51d0f814dbd35
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-06-16 14:18:09 +0600

    new project in mind section complete.

--- src/pages/Home/ProjectIdea/ProjectIdea.js
+++ src/pages/Home/ProjectIdea/ProjectIdea.js
@@ -4,16 +4,14 @@ import style from "./ProjectIdea.module.css";
 const ProjectIdea = () => {
   return (
     <div className={style.projectIdea__container}>
-      {/* <h1>Have a new project in mind ?</h1> */}
-
       <div className={style.text__wrapper}>
         <h1>Have a new project in mind ? </h1>
         <div className={style.words}>
           <span>Let's Talk !</span>
           <span>Start Today !</span>
           <span>We Can Help.</span>
+          <span>Start Today !</span>
           <span>Let's Talk ! </span>
-          <span>We Can Help.</span>
         </div>
         {/* <h3>Developer</h3> */}
       </div>
