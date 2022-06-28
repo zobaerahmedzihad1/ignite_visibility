2079518f538736c87f5131bd4bd7b4f993e8c1de
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-06-28 23:40:43 +0600

    add new navbar

--- src/pages/Home/NavBar/NavBar.js
+++ src/pages/Home/NavBar/NavBar.js
@@ -1,37 +1,63 @@
 import React from "react";
-import { Navbar, Container, Nav } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
 import logo from "../../../assets/logo.png";
 import style from "./NavBar.module.css";
+import {NavLink} from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <>
-      <Navbar className={style.navBar} bg="light" variant="light" sticky="top">
-        <Container>
-          <Navbar.Brand as={NavLink} to="/home" className="fw-bold">
-            <img className={style.logo} src={logo} alt="" />
-          </Navbar.Brand>
-          <Nav className={`${style.navLink} ms-auto`}>
-            <Nav.Link as={NavLink} to="/">
-              Home
-            </Nav.Link>
-
-            <Nav.Link as={NavLink} to="/contact-us">
-              Contact
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/">
-              FAQ
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/contact">
-              Contact Us
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/login">
-              login
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      {["md"].map((expand) => (
+        <Navbar
+          key={expand}
+          bg="light"
+          expand={expand}
+          className="mb-3 px-5 fixed-top"
+        >
+          <Container fluid>
+            <Navbar.Brand as={NavLink} to="/home">
+              <img style={{ width: "85%" }} src={logo} alt="logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  <img className={style.logo} src={logo} alt="logo" />
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3 ">
+                  <Nav.Link as={NavLink} to="/home">Home</Nav.Link>
+                  <Nav.Link as={NavLink} to="/login">login</Nav.Link>
+                  <NavDropdown
+                    title="Admin-Activities"
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    <NavDropdown.Item as={NavLink} to="/contact">contact</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">
+                      Another action
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                      Something else here
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
     </>
   );
 };
