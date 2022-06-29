be36c69bcacbafbd575148ab7e1084c3d58e707a
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-06-29 21:49:33 +0600

    set vertical slider

--- src/pages/Home/Reviews/Review/Review.js
+++ src/pages/Home/Reviews/Review/Review.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ReviewCard from "../ReviewCard/ReviewCard";
+import Slider from "react-slick";
+import { Container } from "react-bootstrap";
 import style from "./Review.module.css";
 
 const Review = () => {
@@ -10,13 +12,37 @@ const Review = () => {
       .then((response) => response.json())
       .then((data) => setReviews(data));
   }, []);
-  
+
+  const settings = {
+    // dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+    // autoplay: true,
+    speed: 1000,
+    // autoplaySpeed: 3000,
+    cssEase: "linear",
+
+    beforeChange: function (currentSlide, nextSlide) {
+      //   console.log("before change", currentSlide, nextSlide);
+    },
+    afterChange: function (currentSlide) {
+      //   console.log("after change", currentSlide);
+    },
+  };
+
   return (
-    <div>
-      <h2>Review</h2>
-      {reviews.map((review) => (
-        <ReviewCard key={review._id} review={review}></ReviewCard>
-      ))}
+    <div className={style.review__container}>
+      <Container >
+      <h2>Vertical Mode with Swipe To Slide</h2>
+      <Slider {...settings}>
+        {reviews.map((review) => (
+          <ReviewCard key={review._id} review={review}></ReviewCard>
+        ))}
+      </Slider>
+    </Container>
     </div>
   );
 };
--- src/pages/Home/Reviews/ReviewCard/ReviewCard.js
+++ src/pages/Home/Reviews/ReviewCard/ReviewCard.js
@@ -1,39 +1,16 @@
 import React from "react";
-import Slider from "react-slick";
+import { Container } from "react-bootstrap";
 import style from "./ReviewCard.module.css";
 
 const ReviewCard = ({ review }) => {
     console.log(review, 'hello');
     const {_id, name, comment} = review;
-    // console.log(title,'review card');
-  const settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    vertical: true,
-    verticalSwiping: true,
-    autoplay: true,
-    speed: 2000,
-    cssEase: "linear",
-
-    beforeChange: function (currentSlide, nextSlide) {
-    //   console.log("before change", currentSlide, nextSlide);
-    },
-    afterChange: function (currentSlide) {
-    //   console.log("after change", currentSlide);
-    },
-  };
+  
   return (
-    <>
-        <h2>Vertical Mode with Swipe To Slide</h2>
-        <Slider {...settings}>
-          <div>
-            <h3>{_id} </h3>
-            <h2>{name} </h2>
-          </div>
-        </Slider>
-    </>
+    <Container className={style.review__card__container}>
+       <h3>Hello vertical slider {_id}</h3>
+       <h2>{name}</h2>
+    </Container>
   );
 };
 
