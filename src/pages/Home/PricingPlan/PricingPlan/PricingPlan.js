2a8bee71af96cc2c2976afabf57c960d73a55cf0
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-06-30 16:22:29 +0600

    pricing title complete.

--- src/pages/Home/PricingPlan/PricingPlan/PricingPlan.js
+++ src/pages/Home/PricingPlan/PricingPlan/PricingPlan.js
@@ -5,11 +5,16 @@ import style from "./PricingPlan.module.css";
 const PricingPlan = () => {
   return (
     <div className={style.pricing__container}>
-      <h3 className="text-center py-5">pricing plan.</h3>
-
-      <div className="text-center">
-        <Link to="/monthly"> monthly </Link>
-        <Link to="/yearly"> yearly </Link>
+      <div className={`${style.pricing__title} text-center`}>
+        <h1>Our Pricing Plans</h1>
+        <h6>
+          Pricing built for businesses of all sizes. Always know what you'll
+          pay.
+        </h6>
+      </div>
+      <div className={`${style.billing__plan} text-center `}>
+        <button className="m-5"><Link className={`${style.monthly__billing}`} to="/monthly"> Billed Monthly </Link></button>
+        <button><Link className={style.yearly__billing} to="/yearly"> Billed Yearly </Link></button>
       </div>
       <Outlet />
     </div>
