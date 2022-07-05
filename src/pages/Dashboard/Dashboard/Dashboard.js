09c74038a766f414e7b679ebcbc35693f92cb0a3
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-07-06 01:29:17 +0600

    add admin access

--- src/pages/Dashboard/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import { NavLink, Outlet } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 import { AiOutlineUnorderedList } from "react-icons/ai";
-import { MdOutlineRateReview } from "react-icons/md";
+import { MdOutlineRateReview, MdAdminPanelSettings } from "react-icons/md";
 import { FiLogOut } from "react-icons/fi";
 import { FaHome } from "react-icons/fa";
 import style from "./Dashboard.module.css";
@@ -15,8 +15,8 @@ const Dashboard = () => {
         <div className={style.dashboard__content}>
           <Row>
             <Col ex={4} sm={4} md={3} lg={3}>
-              <div className={style.routes__container}>
-                <div>
+              <div>
+                <div className={style.routes__container}>
                   <div className={style.dashboard__route}>
                     <NavLink
                       className={(navInfo) =>
@@ -80,6 +80,22 @@ const Dashboard = () => {
                         <FaHome />
                       </span>
                       Back Home
+                    </NavLink>
+                  </div>
+                </div>
+                <div className={style.admin__container}>
+                  <h4>Admin Activities</h4>
+                  <div className={style.dashboard__route}>
+                    <NavLink
+                      className={(navInfo) =>
+                        navInfo.isActive ? style.active : ""
+                      }
+                      to="/make-admin"
+                    >
+                      <span>
+                        <MdAdminPanelSettings />
+                      </span>
+                      Make Admin
                     </NavLink>
                   </div>
                 </div>
